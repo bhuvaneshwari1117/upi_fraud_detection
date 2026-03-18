38b14d2ce6471d45211712e11aa7809a375ba04d
bhuvaneshwari1117 <poojabhuvi1117@gmail.com>
2026-03-18 20:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,28 @@ app = Flask(_name_)
 @app.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect('/predict')
+        return redirect('/home')
     return render_template('login.html')
 
-@app.route('/predict',methods=['GET'])
-def predict():
+@app.route('/home')
+def home():
     return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    upi = request.form.get('upi')
+    amount = request.form.get('amount')
+
+    if amount:
+        amount = float(amount)
+        if amount > 10000:
+            result = "FRAUD ❌"
+        else:
+            result = "SAFE ✅"
+    else:
+        result = "INVALID INPUT"
+
+    return render_template('result.html', result=result)
 
 if _name_ == '_main_':
     app.run()
-    
