ff56ad7f35fdae9044b4f1ca1f50863938c509f8
bhuvaneshwari1117 <poojabhuvi1117@gmail.com>
2026-03-19 19:44:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect
+import os
 import pickle
 import numpy as np
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -32,4 +33,6 @@ def predict():
 
     return render_template('index.html')
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port=int(os.environ.get("PORT",10000)
+    app.run(host="0.0.0.0", port=port)
