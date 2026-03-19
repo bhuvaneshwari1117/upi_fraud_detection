ebf9942994c4a0fcb79a01227cea201b0e2510ea
bhuvaneshwari1117 <poojabhuvi1117@gmail.com>
2026-03-19 21:35:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def predict():
     return render_template('index.html')
 if __name__ == "__main__":
     import os
-    port=int(os.environ.get("PORT",10000)
+    port=int(os.environ.get("PORT",10000))
     app.run(host="0.0.0.0", port=port)
