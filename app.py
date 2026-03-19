3079fe0bf4f5cfadf3cd26dcb61a331d3d413152
bhuvaneshwari v <poojabhuvi1117@gmail.com>
2026-03-19 18:18:12 +0530

    fixed final error

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, request, redirect
 import pickle
 import numpy as np
@@ -32,37 +31,5 @@ def predict():
             return "Error"
 
     return render_template('index.html')
-=======
-from flask import Flask, render_template, request, redirect
-
-app = Flask(_name_)
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return redirect('/home')
-    return render_template('login.html')
-
-@app.route('/home')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods=['POST'])
-def predict():
-    upi = request.form.get('upi')
-    amount = request.form.get('amount')
-
-    if amount:
-        amount = float(amount)
-        if amount > 10000:
-            result = "FRAUD ❌"
-        else:
-            result = "SAFE ✅"
-    else:
-        result = "INVALID INPUT"
-
-    return render_template('result.html', result=result)
-
-if _name_ == '_main_':
-    app.run()
->>>>>>> 38b14d2ce6471d45211712e11aa7809a375ba04d
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
